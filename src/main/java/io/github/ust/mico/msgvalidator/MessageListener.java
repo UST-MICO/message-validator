408abc572f78b6175937cc8ed68236fc45918661
wagnerdk <18245675+wagnerdk@users.noreply.github.com>
2019-06-06 22:53:02 +0200

    Add draft HTML message viewer

--- src/main/java/io/github/ust/mico/msgvalidator/MessageListener.java
+++ src/main/java/io/github/ust/mico/msgvalidator/MessageListener.java
@@ -6,14 +6,18 @@ import io.github.ust.mico.msgvalidator.kafka.MicoCloudEventImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class MessageListener {
 
 
+    /**
+     * Used for messaging via stomp and websockets
+     */
     @Autowired
-    private KafkaConfig kafkaConfig;
+    SimpMessagingTemplate websocketsTemplate;
 
     /**
      * Entry point for incoming messages from kafka.
@@ -23,6 +27,7 @@ public class MessageListener {
     @KafkaListener(topics = "${kafka.input-topic}")
     public void receive(MicoCloudEventImpl<JsonNode> cloudEvent) {
         log.info("Received CloudEvent message: {}", cloudEvent);
+        websocketsTemplate.convertAndSend("/topic/messaging-bridge", cloudEvent);
     }
 
 }
